ebeb3aecf0a6f6cd7bf65f06abe955c188450cb2
danvillecl <144875519+danvillecl@users.noreply.github.com>
2023-10-11 18:03:58 +0530

    commit

--- lmspai2/settings.py
+++ lmspai2/settings.py
@@ -77,19 +77,19 @@ WSGI_APPLICATION = 'lmspai2.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dbmain',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'dbmain',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+ }
 
 
 # Password validation
